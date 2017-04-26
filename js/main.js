404f07648173aed75f0b98dbfda394337a469f82
mattcheang <cheangmatt@gmail.com>
2017-04-25 17:53:43 -0700

    add changes to pages

--- js/main.js
+++ js/main.js
@@ -95,3 +95,13 @@ function consoleText(words, id, colors) {
   }, 400)
 }
 */
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
